4d9298202e2258ca376ab5b2d856701b9c7892a5
Brad Durbin <33232100+braddurbin@users.noreply.github.com>
2026-04-29 13:16:35 -0500

    Adding a programmable macro key

--- keyboards/keychron/q6_pro/ansi_encoder/keymaps/braddurbin/keymap.c
+++ keyboards/keychron/q6_pro/ansi_encoder/keymaps/braddurbin/keymap.c
@@ -32,9 +32,13 @@ enum layers{
   WIN_FN,
 };
 
+enum custom_keycodes {
+    PRG_MAC = NEW_SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_109_ansi(
-        KC_ESC,   KC_F1,     KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,      KC_MUTE,  KC_SNAP,  KC_SIRI,  RGB_TOG,  TO(MAC_BASE),  TO(WIN_BASE),  TO(GAMING),  _______,
+        KC_ESC,   KC_F1,     KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,      KC_MUTE,  KC_SNAP,  KC_SIRI,  RGB_TOG,  TO(MAC_BASE),  TO(WIN_BASE),  TO(GAMING),  PRG_MAC,
         KC_GRV,   KC_1,      KC_2,      KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,   KC_EQL,      KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,        KC_PSLS,       KC_PAST,     KC_PMNS,
         KC_TAB,   KC_Q,      KC_W,      KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,   KC_RBRC,     KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,         KC_P8,         KC_P9,       KC_PPLS,
         KC_F19,   KC_A,      KC_S,      KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,                KC_ENT,                                 KC_P4,         KC_P5,         KC_P6,
@@ -79,6 +83,8 @@ static bool pgdn_active = false;
 static bool pgdn_initial_delay_done = false;
 static bool pgup_active = false;
 static bool pgup_initial_delay_done = false;
+static bool prg_macro_playing = false;
+static bool prg_macro_recording = false;
 static bool q_macro_active = false;
 static bool right_ctrl_active = false;
 static bool win_app_quit = false;
@@ -104,7 +110,34 @@ static bool win_tab_move_left = false;
 static bool win_tab_move_right = false;
 static uint16_t pgdn_timer = 0;
 static uint16_t pgup_timer = 0;
+static uint16_t prg_macro_last_event_time = 0;
 static uint16_t q_macro_timer;
+
+#define PRG_MACRO_MAX_EVENTS 128
+
+typedef struct {
+    uint16_t keycode;
+    bool pressed;
+    uint8_t delay_ms;
+} prg_macro_event_t;
+
+static prg_macro_event_t prg_macro_events[PRG_MACRO_MAX_EVENTS];
+static uint8_t prg_macro_event_count = 0;
+
+static uint16_t prg_macro_resolve_keycode(uint16_t keycode) {
+    switch (keycode) {
+        case KC_LOPTN:
+            return KC_LOPT;
+        case KC_ROPTN:
+            return KC_ROPT;
+        case KC_LCMMD:
+            return KC_LCMD;
+        case KC_RCMMD:
+            return KC_RCMD;
+        default:
+            return keycode;
+    }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
@@ -112,6 +145,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
     #endif
 
+    // Trick to allow the Fn key function as both the Fn key and the menu key.
     if (keycode == MO(MAC_FN) || keycode == MO(WIN_FN)) {
         if (record->event.pressed) {
             fn_key_pressed_alone = true;
@@ -130,6 +164,53 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     fn_key_pressed_alone = false;
+
+    if (keycode == PRG_MAC) {
+        if (!record->event.pressed) {
+            return false;
+        }
+
+        if (layer_state_is(MAC_FN)) {
+            if (!prg_macro_recording) {
+                prg_macro_recording = true;
+                prg_macro_event_count = 0;
+                prg_macro_last_event_time = timer_read();
+            }
+            return false;
+        }
+
+        if (!prg_macro_recording && !prg_macro_playing && prg_macro_event_count > 0) {
+            prg_macro_playing = true;
+            for (uint8_t i = 0; i < prg_macro_event_count; i++) {
+                uint16_t replay_keycode = prg_macro_resolve_keycode(prg_macro_events[i].keycode);
+                wait_ms(prg_macro_events[i].delay_ms);
+                if (prg_macro_events[i].pressed) {
+                    register_code16(replay_keycode);
+                } else {
+                    unregister_code16(replay_keycode);
+                }
+            }
+            prg_macro_playing = false;
+        }
+
+        return false;
+    }
+
+    if (prg_macro_recording && keycode == KC_ESC && record->event.pressed) {
+        prg_macro_recording = false;
+        return true;
+    }
+
+    if (prg_macro_recording && !prg_macro_playing) {
+        if (prg_macro_event_count < PRG_MACRO_MAX_EVENTS) {
+            uint16_t elapsed = timer_elapsed(prg_macro_last_event_time);
+            prg_macro_events[prg_macro_event_count].keycode  = keycode;
+            prg_macro_events[prg_macro_event_count].pressed  = record->event.pressed;
+            prg_macro_events[prg_macro_event_count].delay_ms = elapsed > 255 ? 255 : elapsed;
+            prg_macro_event_count++;
+        }
+        prg_macro_last_event_time = timer_read();
+    }
 
     if (layer_state_is(WIN_BASE)) {
         switch (keycode) {
